cb940f5f74d7c8d914173bfa9e2c1baead87ca77
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-08-24 16:52:46 +0300

    Commit message >>>>> ip range process done <<<<<

--- PortScanner/PostScanner/__init__.py
+++ PortScanner/PostScanner/__init__.py
@@ -1,5 +1,7 @@
 import argparse
+import logging
 import socket
+import sys
 import textwrap
 
 
@@ -9,6 +11,7 @@ class PostScanner:
         self.target_ip = target_ip
         self.target_port = target_port
         self.args = args
+        self.send_packet()
 
     def send_packet(self):
         addr = []
@@ -20,6 +23,49 @@ class PostScanner:
             print(f"Port {addr[1]} Opened")
         except Exception as e:
             print(f"Port {addr[1]} Closed.")
+
+
+def process_hosts(passed_address):
+    # Two varieties of ip addresses that can be entered.
+    # 192.168.1.20
+    # 192.168.1.1-255
+    passed_address.strip()
+    str_ip = ""
+
+    ip_list = passed_address.split('-')
+
+    if len(ip_list) > 2:
+        logging.error("Invalid ip range.")
+        sys.exit(2)
+
+    const_ip = ip_list[0].split('.')
+
+    for num in const_ip[:-1]:
+        str_ip = f"{str_ip}{num}."
+
+    try:
+        if len(ip_list) == 1:
+            return [int(const_ip[-1]), int(const_ip[-1]), str_ip]
+        return [int(const_ip[-1]), int(ip_list[-1]), str_ip]
+    except Exception as e:
+        logging.error("Invalid ip range.")
+        sys.exit(2)
+
+
+def process_ports(passed_port):
+    pass
+
+
+def main(passed_address, passed_port, passes_timeout):
+    # process the targets
+    target_range = process_hosts(passed_address)
+
+    # process the ports
+    ports_range = process_ports(passed_port)
+
+    for target in range(target_range[0], target_range[1] + 1):
+        for port in range(ports_range[0], ports_range[1] + 1):
+            scanner = PostScanner(f"{target_range[2]}{target}", port)
 
 
 if __name__ == "__main__":
@@ -38,4 +84,6 @@ _init__.py -t 10.10.10.1  -p 21
     terminal_arguments.add_argument('-t', '--target', required=True, help='Target(s) to scan.')
     terminal_arguments.add_argument('-s', '--timeout', type=int, default=1, help="Specify The Timeout")
 
-    argument = terminal_arguments.parse_args()
+    arguments = terminal_arguments.parse_args()
+
+    main(arguments.target, arguments.port, arguments.timeout)
